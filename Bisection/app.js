c0db5c4f7edeacf1ad5e7a71f8dc64b54c187acc
RafaelSantosBraz <rafaelsantosbrazpfi@gmail.com>
2019-09-10 10:34:39 -0300

    padronização dos arquivo js e html

--- Bisection/app.js
+++ Bisection/app.js
@@ -1,4 +1,42 @@
+function iniciarBisseccao() {
+    $('#resultado').html("");
+    equacao = $('#equacao').val();
+    a = eval($('#a').val());
+    b = eval($('#b').val());
+    e = eval($('#e').val());
+    aplicarAbordagem(equacao, a, b, e);
+}
 
 function aplicarAbordagem(equacao, a, b, e) {
-    console.log("a");
+    x = a;
+    fa = eval(equacao);
+    x = b;
+    fb = eval(equacao);
+    xm = (a + b) / 2;
+    x = xm;
+    fxm = eval(equacao);
+    adicionarLinha(a, b, fa, fb, xm, fxm);
+    if (!(Math.abs(fxm) <= e)) {
+        if (fxm < 0 && fa < 0) {
+            a = xm;
+        }
+        if (fxm < 0 && fb < 0) {
+            b = xm;
+        }
+        if (fxm > 0 && fa > 0) {
+            a = xm;
+        }
+        if (fxm > 0 && fb > 0) {
+            b = xm;
+        }
+        aplicarAbordagem(equacao, a, b, e);
+    }
+}
+
+function desenharTabela() {
+    //$('#resultado').html("aaaaaa");
+}
+
+function adicionarLinha(a, b, fa, fb, xm, fxm) {
+    $('#resultado').append("a: " + a + "<br>f(a): " + fa + "<br>b: " + b + "<br>f(b): " + fb + "<br>xm: " + xm + "<br>f(xm):" + fxm + "<br><br>");
 }
