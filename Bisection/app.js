287726cafe15e34a1fc409596f847c95d96f0f79
Rafael Dos Santos Braz <CC47192962899@LAB.CCT.UENP.EDU.BR>
2019-09-10 15:18:34 -0300

    correção do algoritmo de Bissecção e adição de método iterativo

--- Bisection/app.js
+++ Bisection/app.js
@@ -1,3 +1,7 @@
+function limparResultado() {
+    $('#resultado').html("");
+}
+
 function iniciarBisseccao() {
     $('#resultado').html("");
     equacao = $('#equacao').val();
@@ -7,34 +11,73 @@ function iniciarBisseccao() {
     aplicarAbordagem(equacao, a, b, e);
 }
 
+function iniciarBisseccaoIterativa() {
+    $('#resultado').html("");
+    equacao = $('#equacao').val();
+    a = eval($('#a').val());
+    b = eval($('#b').val());
+    e = eval($('#e').val());
+    aplicarAbordagemIterativa(equacao, a, b, e);
+}
+
 function aplicarAbordagem(equacao, a, b, e) {
     x = a;
     fa = eval(equacao);
     x = b;
     fb = eval(equacao);
-    xm = (a + b) / 2;
-    x = xm;
-    fxm = eval(equacao);
-    adicionarLinha(a, b, fa, fb, xm, fxm);
-    if (!(Math.abs(fxm) <= e)) {
-        if (fxm < 0 && fa < 0) {
-            a = xm;
+    if (fa * fb < 0) {
+        xm = (a + b) / 2;
+        x = xm;
+        fxm = eval(equacao);
+        adicionarLinha(a, b, fa, fb, xm, fxm);
+        if (!(Math.abs(fxm) <= e)) {
+            if (fxm < 0 && fa < 0) {
+                a = xm;
+            }
+            if (fxm < 0 && fb < 0) {
+                b = xm;
+            }
+            if (fxm > 0 && fa > 0) {
+                a = xm;
+            }
+            if (fxm > 0 && fb > 0) {
+                b = xm;
+            }
+            aplicarAbordagem(equacao, a, b, e);
         }
-        if (fxm < 0 && fb < 0) {
-            b = xm;
-        }
-        if (fxm > 0 && fa > 0) {
-            a = xm;
-        }
-        if (fxm > 0 && fb > 0) {
-            b = xm;
-        }
-        aplicarAbordagem(equacao, a, b, e);
+    } else {
+        $('#resultado').html("Não tem 0 da função no intervalo especificado.");
     }
 }
 
-function desenharTabela() {
-    //$('#resultado').html("aaaaaa");
+function aplicarAbordagemIterativa(equacao, a, b, e) {
+    do {
+        x = a;
+        fa = eval(equacao);
+        x = b;
+        fb = eval(equacao);
+        if (fa * fb < 0) {
+            xm = (a + b) / 2;
+            x = xm;
+            fxm = eval(equacao);
+            adicionarLinha(a, b, fa, fb, xm, fxm);
+            if (fxm < 0 && fa < 0) {
+                a = xm;
+            }
+            if (fxm < 0 && fb < 0) {
+                b = xm;
+            }
+            if (fxm > 0 && fa > 0) {
+                a = xm;
+            }
+            if (fxm > 0 && fb > 0) {
+                b = xm;
+            }
+        }else {
+            $('#resultado').html("Não tem 0 da função no intervalo especificado.");
+            break;
+        }
+    } while (!(Math.abs(fxm) <= e));
 }
 
 function adicionarLinha(a, b, fa, fb, xm, fxm) {
