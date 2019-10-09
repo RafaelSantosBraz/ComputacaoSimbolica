3cda90c10d0e63c4016d4ed3b4ca6140a9054ef8
Rafael Dos Santos Braz <cc47192962899@L1C09X.lab.cct.uenp.edu.br>
2019-10-09 15:09:02 -0300

    obtenção de matriz triangular superior

--- Resolução por LU/app.js
+++ Resolução por LU/app.js
@@ -1,8 +1,13 @@
-var math = require('mathjs')
+var math = require('mathjs');
 
-//const m = [[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 0], [0, 0, 0, 4]]
-const m = [[2, 3, -1], [4, 4, -3], [2, -3, 1]]
+const m = [[2, 3, -1], [4, 4, -3], [2, -3, 1]];
 
-const x = math.lusolve(m, [5, 3, -1])
+// Resolução por LU
+const x = math.lusolve(m, [5, 3, -1]);
 
 console.log(x);
+
+// obtenção da matriz triangular superior
+const ut = math.qr(m);
+
+console.log(ut);
