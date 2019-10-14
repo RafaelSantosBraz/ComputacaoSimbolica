7feffb57d787b51cab55133d0c0de94577abff42
Rafael Dos Santos Braz <cc47192962899@L2C05X.lab.cct.uenp.edu.br>
2019-10-14 15:40:31 -0300

    finalização Gauss-Jacobi

--- Gauss-Jacobi/app.js
+++ Gauss-Jacobi/app.js
@@ -1,24 +1,70 @@
+"use strict";
+
 function iniciarMetodo() {
     let A = [[10, 2, 1], [1, 5, 1], [2, 3, 10]];
-    let B = [7, -8, 6];
-    let funcs = [
-        "(7-2*X[1]-X[2])/10",
-        "(-8-X[0]-X[2])/5",
-        "(6-2*X[0]-3*X[1])/10"
-    ];
-    etapaInicial(A, funcs, B, e);
+    let B = [7, -8, 6];    
+    let e = 0.05;
+    let funcs = inferirFuncoes(A, B);
+    etapaInicial(A, B, funcs, e);
+}
+
+
+function inferirFuncoes(A, B) {
+    let funcs = new Array(A.length);
+    A.forEach((element, index) => {
+        funcs[index] = "1/" + element[index] + "*(" + B[index] + geraSubtracao(element, index) + ")";
+    });
+    return funcs;
+}
+
+function geraSubtracao(element, index) {
+    let res = "";
+    element.forEach((i, j) => {
+        if (index == j);
+        else
+            res += ((-1) * i).toString() + "*X[" + j + "] ";
+    });
+    return res;
 }
 
 function etapaInicial(A, B, funcs, e) {
     let X = new Array(funcs.length);
     for (let i = 0; i < funcs.length; i++) {
-        X[i] = B[i] / A[i][i];        
+        X[i] = B[i] / A[i][i];
     }
-    const result = aplicarAbordagem(A, X, funcs, e);
-    console.log(result);
+    const result = aplicarAbordagem(X, funcs, e);
+    $('#resultado').html(result.toString().split(",").join(", "));
 }
 
-function aplicarAbordagem(A, X, funcs, e) {
+function aplicarAbordagem(X, funcs, e) {
     let Xk = new Array(X.length);
-    
+    for (let i = 0; i < funcs.length; i++) {
+        Xk[i] = eval(funcs[i]);
+    }
+    const XkX = vetMenosVet(Xk, X);
+    const ERx = maiorAbs(XkX) / maiorAbs(X);
+    if (ERx <= e) {
+        return Xk;
+    }
+    return aplicarAbordagem(Xk, funcs, e);
 }
+
+function vetMenosVet(X, Y) {
+    let res = new Array(X.length);
+    for (let i = 0; i < X.length; i++) {
+        res[i] = Math.abs(Math.abs(X[i]) - Math.abs(Y[i]));
+    }
+    return res;
+}
+
+function maiorAbs(vec) {
+    let aux = -Infinity;
+    // console.log(vec);
+    vec.forEach(i => {
+        if (Math.abs(i) > aux) {
+            aux = i;
+        }
+    });
+    return aux;
+}
+
